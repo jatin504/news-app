a1dded763424746c437fedc1e469c763d0e3f4e9
jatin504 <jt872678@gmail.com>
2024-04-19 19:16:07 +0530

    link update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "baf74fd87c284295b84739079d44772c";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
